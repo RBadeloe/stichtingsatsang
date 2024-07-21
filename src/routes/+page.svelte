8f4d19289a12ded9b199c424bf97720f8bd7fdfc
Robert Badeloe <robert.badeloe@live.nl>
2024-07-21 04:49:42 +0200

    added facebook

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -59,6 +59,9 @@
       <a href="https://www.instagram.com/stichtingsatsangnederland/"
         ><img src="../assets/instagram.svg" alt="" /></a
       >
+      <a href="https://facebook.com/stichtingsatsangnederland/"
+        ><img src="../assets/facebook.svg" alt="" /></a
+      >
       <a href="mailto:satsangned@gmail.com"
         ><img src="../assets/mail.svg" alt="" /></a
       >
